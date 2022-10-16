74560086840fe04fa32096b03dd3616ab45fd5df
Emily London <emilylondon@Emilys-MacBook-Pro.local>
2022-10-16 08:26:46 -0700

    please

--- hardware/drive.py
+++ hardware/drive.py
@@ -6,40 +6,47 @@ LEFT_REVERSE = 19 #IN2
 RIGHT_FORWARD = 13
 RIGHT_REVERSE = 6
 
-def robo_init():
-    forwardLeft = PWMOutputDevice(LEFT_FORWARD, True, 0, 1000)
-    reverseLeft = PWMOutputDevice(LEFT_REVERSE, True, 0, 1000)  
-    forwardRight = PWMOutputDevice(RIGHT_FORWARD, True, 0, 1000)
-    reverseRight = PWMOutputDevice(RIGHT_REVERSE, True, 0, 1000)   
+class Robot:
+    def __init__(self):
+        self.forwardLeft = PWMOutputDevice(LEFT_FORWARD, True, 0, 1000)
+        self.reverseLeft = PWMOutputDevice(LEFT_REVERSE, True, 0, 1000)  
+        self.forwardRight = PWMOutputDevice(RIGHT_FORWARD, True, 0, 1000)
+        self.everseRight = PWMOutputDevice(RIGHT_REVERSE, True, 0, 1000)   
+        
+    def robo_init():
+        forwardLeft = PWMOutputDevice(LEFT_FORWARD, True, 0, 1000)
+        reverseLeft = PWMOutputDevice(LEFT_REVERSE, True, 0, 1000)  
+        forwardRight = PWMOutputDevice(RIGHT_FORWARD, True, 0, 1000)
+        reverseRight = PWMOutputDevice(RIGHT_REVERSE, True, 0, 1000)   
 
-def forward(o):
-    forwardLeft.value = 1.0*(1+o)
-    reverseLeft.value = 0
-    forwardRight.value = 1.0*(1-o)
-    reverseRight.value = 0
+    def forward(o):
+        forwardLeft.value = 1.0*(1+o)
+        reverseLeft.value = 0
+        forwardRight.value = 1.0*(1-o)
+        reverseRight.value = 0
 
-def left_turn():
-    halt()
-    forwardLeft.value = 0.0
-    reverseLeft.value = 1.0
-    forwardRight.value = 1.0
-    reverseRight.value = 0
-    time.sleep(1)
-    halt()
+    def left_turn():
+        halt()
+        forwardLeft.value = 0.0
+        reverseLeft.value = 1.0
+        forwardRight.value = 1.0
+        reverseRight.value = 0
+        time.sleep(1)
+        halt()
 
-def right_turn():
-    halt()
-    forwardLeft.value = 1.0
-    reverseLeft.value = 0
-    forwardRight.value = 0
-    reverseRight.value = 1.0
-    time.sleep(1)
-    halt()
+    def right_turn():
+        halt()
+        forwardLeft.value = 1.0
+        reverseLeft.value = 0
+        forwardRight.value = 0
+        reverseRight.value = 1.0
+        time.sleep(1)
+        halt()
 
-def halt():
-    forwardLeft.value = 0
-    reverseLeft.value = 0
-    forwardRight.value = 0
-    reverseRight.value = 0
+    def halt():
+        forwardLeft.value = 0
+        reverseLeft.value = 0
+        forwardRight.value = 0
+        reverseRight.value = 0
 
     
