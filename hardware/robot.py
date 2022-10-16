6179e77b013e912e499c44553ca07535a3c1a319
Emily London <emilylondon@Emilys-MacBook-Pro.local>
2022-10-16 10:47:32 -0700

    Refine PD

--- hardware/robot.py
+++ hardware/robot.py
@@ -202,7 +202,7 @@ if __name__ == '__main__':
                             robot.right_turn()
                             robot.right_turn()
                         orr = S 
-                    robot.forward(pdcont.update(theta))
+                    robot.forward(0)#pdcont.update(theta))
                     client.publish("chargr/loc", str(int(x/14)) + ',' + str(int(y/11)))
                 else: 
                     robot.halt()
