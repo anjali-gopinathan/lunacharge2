099fdeb5882ffffbff9ad11c8cab91bc51f903c4
Emily London <emilylondon@Emilys-MacBook-Pro.local>
2022-10-16 06:46:49 -0700

    added pd controller

---
+++ hardware/pdController.py
@@ -0,0 +1,15 @@
+import time 
+
+class PDController:
+    def __init__(self, target, kp, kd):
+        self.target = target 
+        self.kp = kp
+        self.kd = kd 
+        self.oldError = 0
+        self.oldTime = time.time()
+
+    def update(value):
+        err = value - target
+        newTime = time.time()
+        o = err*kp + (err-oldError)/(newTime-oldTime)*kd
+        oldTime = newTime
